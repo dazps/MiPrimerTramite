9c52900d03fab252e8fd9f43e2b9ca2543c59a8d
Eder Mauricio Cornejo <u202212471@upc.edu.pe>
2025-09-17 23:09:20 -0500

    Init

---
+++ src/main/java/com/example/miprimertramitegithub2025/MiPrimerTramiteGitHub2025Application.java
@@ -0,0 +1,13 @@
+package com.example.miprimertramitegithub2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimerTramiteGitHub2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiPrimerTramiteGitHub2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miprimertramitegithub2025/MiPrimerTramiteGitHub2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miprimertramitegithub2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimerTramiteGitHub2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
