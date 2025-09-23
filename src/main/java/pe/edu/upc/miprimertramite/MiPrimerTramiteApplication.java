10abbe7118ec1f44b7fb5fff478e1cbc388f9259
Dalí Paredes <dazps05@gmail.com>
2025-09-23 08:45:56 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/miprimertramite/MiPrimerTramiteApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.miprimertramite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimerTramiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiPrimerTramiteApplication.class, args);
+    }
+
+}
