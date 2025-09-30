0dc8b54a372eaa1c66181819ded5687b1392381d
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:53:06 -0500

    Update NotificacionServiceImplement.java

--- src/main/java/pe/edu/upc/miprimertramite/servicesimplements/NotificacionServiceImplement.java
+++ src/main/java/pe/edu/upc/miprimertramite/servicesimplements/NotificacionServiceImplement.java
@@ -1,4 +1,46 @@
-package pe.edu.upc.miprimertramite.servicesimplements;
+package pe.edu.upc.miprimertramite.servicesimplementations;
 
-public class NotificacionServiceImplement {
+import pe.edu.upc.miprimertramite.entities.Notificacion;
+import pe.edu.upc.miprimertramite.repositories.INotificacionRepository;
+import pe.edu.upc.miprimertramite.servicesinterfaces.INotificacionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NotificacionServiceImplement implements INotificacionService {
+
+    @Autowired
+    private INotificacionRepository notificacionRepository;
+
+    @Override
+    public Notificacion save(Notificacion notificacion) {
+        return notificacionRepository.save(notificacion);
+    }
+
+    @Override
+    public List<Notificacion> list() {
+        return notificacionRepository.findAll();
+    }
+
+    @Override
+    public Notificacion findById(int id) {
+        return notificacionRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void delete(int id) {
+        notificacionRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Notificacion> findPendingByUsuarioId(int idUsuario) {
+        return notificacionRepository.findPendingByUsuarioId(idUsuario);
+    }
+
+    @Override
+    public Notificacion markAsSent(int idNotificacion) {
+        return notificacionRepository.markAsSent(idNotificacion);
+    }
 }
