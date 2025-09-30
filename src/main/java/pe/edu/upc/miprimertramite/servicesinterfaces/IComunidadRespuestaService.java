01fff965f95ac480ceb444a9265405685016c256
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:39:05 -0500

    Update IComunidadRespuestaService.java

--- src/main/java/pe/edu/upc/miprimertramite/servicesinterfaces/IComunidadRespuestaService.java
+++ src/main/java/pe/edu/upc/miprimertramite/servicesinterfaces/IComunidadRespuestaService.java
@@ -1,4 +1,13 @@
 package pe.edu.upc.miprimertramite.servicesinterfaces;
 
+import pe.edu.upc.miprimertramite.entities.ComunidadRespuesta;
+import java.util.List;
+
 public interface IComunidadRespuestaService {
+    ComunidadRespuesta save(ComunidadRespuesta respuesta);
+    List<ComunidadRespuesta> list();
+    ComunidadRespuesta findById(int id);
+    void delete(int id);
+    List<ComunidadRespuesta> findByPreguntaId(int idPregunta);
+    Long countByPreguntaId(int idPregunta);
 }
