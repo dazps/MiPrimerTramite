4aa2aa3a0d1e285de70acae1bc60c0fb09502b24
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:53:19 -0500

    Update TipoNotificacionServiceImplement.java

--- src/main/java/pe/edu/upc/miprimertramite/servicesimplements/TipoNotificacionServiceImplement.java
+++ src/main/java/pe/edu/upc/miprimertramite/servicesimplements/TipoNotificacionServiceImplement.java
@@ -1,4 +1,41 @@
-package pe.edu.upc.miprimertramite.servicesimplements;
+package pe.edu.upc.miprimertramite.servicesimplementations;
 
-public class TipoNotificacionServiceImplement {
+import pe.edu.upc.miprimertramite.entities.TipoNotificacion;
+import pe.edu.upc.miprimertramite.repositories.ITipoNotificacionRepository;
+import pe.edu.upc.miprimertramite.servicesinterfaces.ITipoNotificacionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TipoNotificacionServiceImpl implements ITipoNotificacionService {
+
+    @Autowired
+    private ITipoNotificacionRepository tipoNotificacionRepository;
+
+    @Override
+    public TipoNotificacion save(TipoNotificacion tipo) {
+        return tipoNotificacionRepository.save(tipo);
+    }
+
+    @Override
+    public List<TipoNotificacion> list() {
+        return tipoNotificacionRepository.findAll();
+    }
+
+    @Override
+    public TipoNotificacion findById(int id) {
+        return tipoNotificacionRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void delete(int id) {
+        tipoNotificacionRepository.deleteById(id);
+    }
+
+    @Override
+    public TipoNotificacion findByNombre(String nombre) {
+        return tipoNotificacionRepository.findByNombre(nombre);
+    }
 }
