cd5d78c429f484d62abbeb12e7f5f7ea65f8b5d8
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:38:24 -0500

    Update IActividadSistemaService.java

--- src/main/java/pe/edu/upc/miprimertramite/servicesinterfaces/IActividadSistemaService.java
+++ src/main/java/pe/edu/upc/miprimertramite/servicesinterfaces/IActividadSistemaService.java
@@ -1,4 +1,13 @@
 package pe.edu.upc.miprimertramite.servicesinterfaces;
 
+import pe.edu.upc.miprimertramite.entities.ActividadSistema;
+import java.util.List;
+
 public interface IActividadSistemaService {
+    ActividadSistema save(ActividadSistema actividad);
+    List<ActividadSistema> list();
+    ActividadSistema findById(int id);
+    void delete(int id);
+    List<ActividadSistema> findLatest10ByUsuarioId(int idUsuario);
+    Long countByUsuarioAndTipo(int idUsuario, String tipoActividad);
 }
