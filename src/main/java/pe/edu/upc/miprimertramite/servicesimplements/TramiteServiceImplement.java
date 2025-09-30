e6c319158fb3d402b2f227dbed99f5f6099ce7d5
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:54:04 -0500

    Update TramiteServiceImplement.java

--- src/main/java/pe/edu/upc/miprimertramite/servicesimplements/TramiteServiceImplement.java
+++ src/main/java/pe/edu/upc/miprimertramite/servicesimplements/TramiteServiceImplement.java
@@ -1,4 +1,51 @@
-package pe.edu.upc.miprimertramite.servicesimplements;
+package pe.edu.upc.miprimertramite.servicesimplementations;
 
-public class TramiteServiceImplement {
+import pe.edu.upc.miprimertramite.entities.Tramite;
+import pe.edu.upc.miprimertramite.repositories.ITramiteRepository;
+import pe.edu.upc.miprimertramite.servicesinterfaces.ITramiteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TramiteServiceImpl implements ITramiteService {
+
+    @Autowired
+    private ITramiteRepository tramiteRepository;
+
+    @Override
+    public Tramite save(Tramite tramite) {
+        return tramiteRepository.save(tramite);
+    }
+
+    @Override
+    public List<Tramite> list() {
+        return tramiteRepository.findAll();
+    }
+
+    @Override
+    public Tramite findById(int id) {
+        return tramiteRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void delete(int id) {
+        tramiteRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Tramite> findTop10ByPopularidad() {
+        return tramiteRepository.findTop10ByPopularidad();
+    }
+
+    @Override
+    public List<Tramite> findByMinPuntuacion(int minPuntuacion) {
+        return tramiteRepository.findByMinPuntuacion(minPuntuacion);
+    }
+
+    @Override
+    public Double findAveragePuntuacionByTramiteId(int idTramite) {
+        return tramiteRepository.findAveragePuntuacionByTramiteId(idTramite);
+    }
 }
