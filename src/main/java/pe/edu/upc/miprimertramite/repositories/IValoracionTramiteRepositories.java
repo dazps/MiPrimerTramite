6fbf01c445ae3e0d2c4e714131f9ac55d450a666
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:11:48 -0500

    Update IValoracionTramiteRepositories.java

--- src/main/java/pe/edu/upc/miprimertramite/repositories/IValoracionTramiteRepositories.java
+++ src/main/java/pe/edu/upc/miprimertramite/repositories/IValoracionTramiteRepositories.java
@@ -1,4 +1,21 @@
 package pe.edu.upc.miprimertramite.repositories;
 
-public interface IValoracionTramiteRepositories {
+import pe.edu.upc.miprimertramite.entities.ValoracionTramite;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IValoracionTramiteRepository extends JpaRepository<ValoracionTramite, Integer> {
+
+    @Query(value = "SELECT * FROM Valoraciones_Tramites WHERE ID_Tramite = ?1", nativeQuery = true)
+    List<ValoracionTramite> findByTramiteId(int idTramite);
+
+    @Query(value = "SELECT AVG(Puntuacion) FROM Valoraciones_Tramites WHERE ID_Tramite = ?1", nativeQuery = true)
+    Double findAveragePuntuacionByTramiteId(int idTramite);
+
+    @Query(value = "SELECT COUNT(*) FROM Valoraciones_Tramites WHERE ID_Tramite = ?1", nativeQuery = true)
+    Long countByTramiteId(int idTramite);
 }
