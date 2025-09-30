357ddf1eaf4691bd68577192bf27b4bcbd5a3949
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:41:57 -0500

    Update ITramitePasoService.java

--- src/main/java/pe/edu/upc/miprimertramite/servicesinterfaces/ITramitePasoService.java
+++ src/main/java/pe/edu/upc/miprimertramite/servicesinterfaces/ITramitePasoService.java
@@ -1,4 +1,13 @@
 package pe.edu.upc.miprimertramite.servicesinterfaces;
 
+import pe.edu.upc.miprimertramite.entities.TramitePaso;
+import java.util.List;
+
 public interface ITramitePasoService {
+    TramitePaso save(TramitePaso paso);
+    List<TramitePaso> list();
+    TramitePaso findById(int id);
+    void delete(int id);
+    List<TramitePaso> findByTramiteId(int idTramite);
+    List<TramitePaso> findByTramiteIdOrdered(int idTramite);
 }
