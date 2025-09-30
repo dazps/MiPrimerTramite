2a0475f88388b324a675c3bac274081108afd1f6
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:45:35 -0500

    Update IUsuarioTramiteProgresoService.java

--- src/main/java/pe/edu/upc/miprimertramite/servicesinterfaces/IUsuarioTramiteProgresoService.java
+++ src/main/java/pe/edu/upc/miprimertramite/servicesinterfaces/IUsuarioTramiteProgresoService.java
@@ -1,4 +1,14 @@
 package pe.edu.upc.miprimertramite.servicesinterfaces;
 
+import pe.edu.upc.miprimertramite.entities.UsuarioTramiteProgreso;
+import java.util.List;
+
 public interface IUsuarioTramiteProgresoService {
+    UsuarioTramiteProgreso save(UsuarioTramiteProgreso progreso);
+    List<UsuarioTramiteProgreso> list();
+    UsuarioTramiteProgreso findById(int id);
+    void delete(int id);
+    List<UsuarioTramiteProgreso> findByUsuarioAndTramite(int idUsuario, int idTramite);
+    Long countCompletedStepsByUsuarioAndTramite(int idUsuario, int idTramite);
+    UsuarioTramiteProgreso findNextStep(int idUsuario, int idTramite);
 }
