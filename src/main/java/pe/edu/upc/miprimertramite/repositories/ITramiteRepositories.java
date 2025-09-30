610a11d6168bf601086543e5b307ce1d04e43771
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:10:40 -0500

    Update ITramiteRepositories.java

--- src/main/java/pe/edu/upc/miprimertramite/repositories/ITramiteRepositories.java
+++ src/main/java/pe/edu/upc/miprimertramite/repositories/ITramiteRepositories.java
@@ -1,4 +1,21 @@
 package pe.edu.upc.miprimertramite.repositories;
 
-public interface ITramiteRepositories {
+import pe.edu.upc.miprimertramite.entities.Tramite;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ITramiteRepository extends JpaRepository<Tramite, Integer> {
+
+    @Query(value = "SELECT * FROM Tramites ORDER BY Popularidad DESC LIMIT 10", nativeQuery = true)
+    List<Tramite> findTop10ByPopularidad();
+
+    @Query(value = "SELECT t.* FROM Tramites t JOIN Valoraciones_Tramites v ON t.ID_Tramite = v.ID_Tramite WHERE v.Puntuacion >= ?1", nativeQuery = true)
+    List<Tramite> findByMinPuntuacion(int minPuntuacion);
+
+    @Query(value = "SELECT AVG(v.Puntuacion) FROM Valoraciones_Tramites v WHERE v.ID_Tramite = ?1", nativeQuery = true)
+    Double findAveragePuntuacionByTramiteId(int idTramite);
 }
