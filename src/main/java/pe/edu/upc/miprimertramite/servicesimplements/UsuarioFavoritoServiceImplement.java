f19cf23151ae854311b8dcf6b3830d07c8471cf5
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:54:18 -0500

    Update UsuarioFavoritoServiceImplement.java

--- src/main/java/pe/edu/upc/miprimertramite/servicesimplements/UsuarioFavoritoServiceImplement.java
+++ src/main/java/pe/edu/upc/miprimertramite/servicesimplements/UsuarioFavoritoServiceImplement.java
@@ -1,4 +1,51 @@
-package pe.edu.upc.miprimertramite.servicesimplements;
+package pe.edu.upc.miprimertramite.servicesimplementations;
 
-public class UsuarioFavoritoServiceImplement {
+import pe.edu.upc.miprimertramite.entities.UsuarioFavorito;
+import pe.edu.upc.miprimertramite.repositories.IUsuarioFavoritoRepository;
+import pe.edu.upc.miprimertramite.servicesinterfaces.IUsuarioFavoritoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioFavoritoServiceImpl implements IUsuarioFavoritoService {
+
+    @Autowired
+    private IUsuarioFavoritoRepository usuarioFavoritoRepository;
+
+    @Override
+    public UsuarioFavorito save(UsuarioFavorito favorito) {
+        return usuarioFavoritoRepository.save(favorito);
+    }
+
+    @Override
+    public List<UsuarioFavorito> list() {
+        return usuarioFavoritoRepository.findAll();
+    }
+
+    @Override
+    public UsuarioFavorito findById(int id) {
+        return usuarioFavoritoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void delete(int id) {
+        usuarioFavoritoRepository.deleteById(id);
+    }
+
+    @Override
+    public List<UsuarioFavorito> findByUsuarioId(int idUsuario) {
+        return usuarioFavoritoRepository.findByUsuarioId(idUsuario);
+    }
+
+    @Override
+    public Long countByTramiteId(int idTramite) {
+        return usuarioFavoritoRepository.countByTramiteId(idTramite);
+    }
+
+    @Override
+    public UsuarioFavorito findByUsuarioAndTramite(int idUsuario, int idTramite) {
+        return usuarioFavoritoRepository.findByUsuarioAndTramite(idUsuario, idTramite);
+    }
 }
