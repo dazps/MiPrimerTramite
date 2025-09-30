727fc1b9828bd46c2c312dc77477e02b3af1d347
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:08:52 -0500

    Update INotificacionRepositories.java

--- src/main/java/pe/edu/upc/miprimertramite/repositories/INotificacionRepositories.java
+++ src/main/java/pe/edu/upc/miprimertramite/repositories/INotificacionRepositories.java
@@ -1,4 +1,18 @@
 package pe.edu.upc.miprimertramite.repositories;
 
-public interface INotificacionRepositories {
+import pe.edu.upc.miprimertramite.entities.Notificacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface INotificacionRepository extends JpaRepository<Notificacion, Integer> {
+
+    @Query(value = "SELECT * FROM Notificaciones WHERE ID_Usuario = ?1 AND Enviado = FALSE", nativeQuery = true)
+    List<Notificacion> findPendingByUsuarioId(int idUsuario);
+
+    @Query(value = "UPDATE Notificaciones SET Enviado = TRUE WHERE ID_Notificacion = ?1 RETURNING *", nativeQuery = true)
+    Notificacion markAsSent(int idNotificacion);
 }
