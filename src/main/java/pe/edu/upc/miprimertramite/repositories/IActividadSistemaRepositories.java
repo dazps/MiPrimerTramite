368aeb727915da8496a2272fd212ceda8ca9989e
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:03:12 -0500

    Update IActividadSistemaRepositories.java

--- src/main/java/pe/edu/upc/miprimertramite/repositories/IActividadSistemaRepositories.java
+++ src/main/java/pe/edu/upc/miprimertramite/repositories/IActividadSistemaRepositories.java
@@ -1,4 +1,18 @@
 package pe.edu.upc.miprimertramite.repositories;
 
-public interface IActividadSistemaRepositories {
+import pe.edu.upc.miprimertramite.entities.ActividadSistema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IActividadSistemaRepository extends JpaRepository<ActividadSistema, Integer> {
+
+    @Query(value = "SELECT * FROM Actividad_Sistema WHERE ID_Usuario = ?1 ORDER BY Fecha DESC LIMIT 10", nativeQuery = true)
+    List<ActividadSistema> findLatest10ByUsuarioId(int idUsuario);
+
+    @Query(value = "SELECT COUNT(*) FROM Actividad_Sistema WHERE ID_Usuario = ?1 AND TipoActividad = ?2", nativeQuery = true)
+    Long countByUsuarioAndTipo(int idUsuario, String tipoActividad);
 }
