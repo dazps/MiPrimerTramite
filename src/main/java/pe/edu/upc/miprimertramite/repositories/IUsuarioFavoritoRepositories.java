cf969820939097950439f6b2a65caa388c76299f
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:10:50 -0500

    Update IUsuarioFavoritoRepositories.java

--- src/main/java/pe/edu/upc/miprimertramite/repositories/IUsuarioFavoritoRepositories.java
+++ src/main/java/pe/edu/upc/miprimertramite/repositories/IUsuarioFavoritoRepositories.java
@@ -1,4 +1,21 @@
 package pe.edu.upc.miprimertramite.repositories;
 
-public interface IUsuarioFavoritoRepositories {
+import pe.edu.upc.miprimertramite.entities.UsuarioFavorito;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IUsuarioFavoritoRepository extends JpaRepository<UsuarioFavorito, Integer> {
+
+    @Query(value = "SELECT * FROM Usuario_Favoritos WHERE ID_Usuario = ?1", nativeQuery = true)
+    List<UsuarioFavorito> findByUsuarioId(int idUsuario);
+
+    @Query(value = "SELECT COUNT(*) FROM Usuario_Favoritos WHERE ID_Tramite = ?1", nativeQuery = true)
+    Long countByTramiteId(int idTramite);
+
+    @Query(value = "SELECT * FROM Usuario_Favoritos WHERE ID_Usuario = ?1 AND ID_Tramite = ?2", nativeQuery = true)
+    UsuarioFavorito findByUsuarioAndTramite(int idUsuario, int idTramite);
 }
