9e2ecb2b6ec62eb4139447dd652e661d87a3426d
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:53:40 -0500

    Update TramiteDocumentoServiceImplement.java

--- src/main/java/pe/edu/upc/miprimertramite/servicesimplements/TramiteDocumentoServiceImplement.java
+++ src/main/java/pe/edu/upc/miprimertramite/servicesimplements/TramiteDocumentoServiceImplement.java
@@ -1,4 +1,46 @@
-package pe.edu.upc.miprimertramite.servicesimplements;
+package pe.edu.upc.miprimertramite.servicesimplementations;
 
-public class TramiteDocumentoServiceImplement {
+import pe.edu.upc.miprimertramite.entities.TramiteDocumento;
+import pe.edu.upc.miprimertramite.repositories.ITramiteDocumentoRepository;
+import pe.edu.upc.miprimertramite.servicesinterfaces.ITramiteDocumentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TramiteDocumentoServiceImpl implements ITramiteDocumentoService {
+
+    @Autowired
+    private ITramiteDocumentoRepository tramiteDocumentoRepository;
+
+    @Override
+    public TramiteDocumento save(TramiteDocumento documento) {
+        return tramiteDocumentoRepository.save(documento);
+    }
+
+    @Override
+    public List<TramiteDocumento> list() {
+        return tramiteDocumentoRepository.findAll();
+    }
+
+    @Override
+    public TramiteDocumento findById(int id) {
+        return tramiteDocumentoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void delete(int id) {
+        tramiteDocumentoRepository.deleteById(id);
+    }
+
+    @Override
+    public List<TramiteDocumento> findByTramiteId(int idTramite) {
+        return tramiteDocumentoRepository.findByTramiteId(idTramite);
+    }
+
+    @Override
+    public Long countRequiredDocumentsByTramiteId(int idTramite) {
+        return tramiteDocumentoRepository.countRequiredDocumentsByTramiteId(idTramite);
+    }
 }
