6a84f3b3f22c50a9539d8564c4c64611ed607525
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:09:57 -0500

    Update ITramiteDocumentoRepositories.java

--- src/main/java/pe/edu/upc/miprimertramite/repositories/ITramiteDocumentoRepositories.java
+++ src/main/java/pe/edu/upc/miprimertramite/repositories/ITramiteDocumentoRepositories.java
@@ -1,4 +1,18 @@
 package pe.edu.upc.miprimertramite.repositories;
 
-public interface ITramiteDocumentoRepositories {
+import pe.edu.upc.miprimertramite.entities.TramiteDocumento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ITramiteDocumentoRepository extends JpaRepository<TramiteDocumento, Integer> {
+
+    @Query(value = "SELECT * FROM Tramite_Documentos WHERE ID_Tramite = ?1", nativeQuery = true)
+    List<TramiteDocumento> findByTramiteId(int idTramite);
+
+    @Query(value = "SELECT COUNT(*) FROM Tramite_Documentos WHERE ID_Tramite = ?1 AND EsOpcional = FALSE", nativeQuery = true)
+    Long countRequiredDocumentsByTramiteId(int idTramite);
 }
