2db1a8cdc7fa06d1c0c957eb799408e1eedf0df8
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:41:47 -0500

    Update ITramiteDocumentoService.java

--- src/main/java/pe/edu/upc/miprimertramite/servicesinterfaces/ITramiteDocumentoService.java
+++ src/main/java/pe/edu/upc/miprimertramite/servicesinterfaces/ITramiteDocumentoService.java
@@ -1,4 +1,13 @@
 package pe.edu.upc.miprimertramite.servicesinterfaces;
 
+import pe.edu.upc.miprimertramite.entities.TramiteDocumento;
+import java.util.List;
+
 public interface ITramiteDocumentoService {
+    TramiteDocumento save(TramiteDocumento documento);
+    List<TramiteDocumento> list();
+    TramiteDocumento findById(int id);
+    void delete(int id);
+    List<TramiteDocumento> findByTramiteId(int idTramite);
+    Long countRequiredDocumentsByTramiteId(int idTramite);
 }
