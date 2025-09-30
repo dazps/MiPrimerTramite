59e1e292a81a809c4360fe1f3635950470193da1
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:55:29 -0500

    Update ValoracionTramiteServiceImplement.java

--- src/main/java/pe/edu/upc/miprimertramite/servicesimplements/ValoracionTramiteServiceImplement.java
+++ src/main/java/pe/edu/upc/miprimertramite/servicesimplements/ValoracionTramiteServiceImplement.java
@@ -1,4 +1,51 @@
-package pe.edu.upc.miprimertramite.servicesimplements;
+package pe.edu.upc.miprimertramite.servicesimplementations;
 
-public class ValoracionTramiteServiceImplement {
+import pe.edu.upc.miprimertramite.entities.ValoracionTramite;
+import pe.edu.upc.miprimertramite.repositories.IValoracionTramiteRepository;
+import pe.edu.upc.miprimertramite.servicesinterfaces.IValoracionTramiteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ValoracionTramiteServiceImpl implements IValoracionTramiteService {
+
+    @Autowired
+    private IValoracionTramiteRepository valoracionTramiteRepository;
+
+    @Override
+    public ValoracionTramite save(ValoracionTramite valoracion) {
+        return valoracionTramiteRepository.save(valoracion);
+    }
+
+    @Override
+    public List<ValoracionTramite> list() {
+        return valoracionTramiteRepository.findAll();
+    }
+
+    @Override
+    public ValoracionTramite findById(int id) {
+        return valoracionTramiteRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void delete(int id) {
+        valoracionTramiteRepository.deleteById(id);
+    }
+
+    @Override
+    public List<ValoracionTramite> findByTramiteId(int idTramite) {
+        return valoracionTramiteRepository.findByTramiteId(idTramite);
+    }
+
+    @Override
+    public Double findAveragePuntuacionByTramiteId(int idTramite) {
+        return valoracionTramiteRepository.findAveragePuntuacionByTramiteId(idTramite);
+    }
+
+    @Override
+    public Long countByTramiteId(int idTramite) {
+        return valoracionTramiteRepository.countByTramiteId(idTramite);
+    }
 }
