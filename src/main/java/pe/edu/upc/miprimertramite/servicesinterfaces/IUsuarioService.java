332a253cae04ca717f6d8441320b1187273e62a2
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:45:25 -0500

    Update IUsuarioService.java

--- src/main/java/pe/edu/upc/miprimertramite/servicesinterfaces/IUsuarioService.java
+++ src/main/java/pe/edu/upc/miprimertramite/servicesinterfaces/IUsuarioService.java
@@ -1,4 +1,14 @@
 package pe.edu.upc.miprimertramite.servicesinterfaces;
 
+import pe.edu.upc.miprimertramite.entities.Usuario;
+import java.util.List;
+
 public interface IUsuarioService {
+    Usuario save(Usuario usuario);
+    List<Usuario> list();
+    Usuario findById(int id);
+    void delete(int id);
+    Usuario findByEmail(String email);
+    List<Usuario> findByTipoUsuarioNombre(String nombreTipo);
+    List<Usuario> findByTipoUsuarioId(int idTipoUsuario);
 }
