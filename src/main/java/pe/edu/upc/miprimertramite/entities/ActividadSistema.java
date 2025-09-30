fe8e5bb1d36c3314d656bd686f6c24a626a6d22f
Dalí Paredes <dazps05@gmail.com>
2025-09-29 23:44:25 -0500

    adding entities ActividadSistema and TramiteDocumento

--- src/main/java/pe/edu/upc/miprimertramite/entities/ActividadSistema.java
+++ src/main/java/pe/edu/upc/miprimertramite/entities/ActividadSistema.java
@@ -1,4 +1,78 @@
 package pe.edu.upc.miprimertramite.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ActividadSistema")
 public class ActividadSistema {
+
+    @Id
+    @GeneratedValue
+    private int idActividad;
+
+    @Column(name = "TipoActividad", length = 50, nullable = false)
+    private String tipoActividad;
+
+    @Column(name = "Descripcion", length = 255, nullable = false)
+    private String descripcion;
+
+    @Column(name = "Fecha", nullable = false)
+    private LocalDateTime fecha;
+
+    @ManyToOne
+    @JoinColumn(name = "idUsuario")
+    private Usuario usuario;
+
+    public ActividadSistema(){
+    }
+
+    public ActividadSistema(int idActividad, String tipoActividad, String descripcion, LocalDateTime fecha, Usuario usuario) {
+        this.idActividad = idActividad;
+        this.tipoActividad = tipoActividad;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.usuario = usuario;
+    }
+
+    public int getIdActividad() {
+        return idActividad;
+    }
+
+    public void setIdActividad(int idActividad) {
+        this.idActividad = idActividad;
+    }
+
+    public String getTipoActividad() {
+        return tipoActividad;
+    }
+
+    public void setTipoActividad(String tipoActividad) {
+        this.tipoActividad = tipoActividad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
--- src/main/java/pe/edu/upc/miprimertramite/entities/TramiteDocumento.java
+++ src/main/java/pe/edu/upc/miprimertramite/entities/TramiteDocumento.java
@@ -1,4 +1,65 @@
 package pe.edu.upc.miprimertramite.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TramiteDocumento")
 public class TramiteDocumento {
+
+    @Id
+    @GeneratedValue
+    private int idDocumento;
+
+    @Column(name = "Nombre", length = 150, nullable = false)
+    private String nombre;
+
+    @Column(name = "EsOpcional", nullable = false)
+    private Boolean esOpcional;
+
+    @ManyToOne
+    @JoinColumn(name = "idTramite")
+    private Tramite tramite;
+
+    public TramiteDocumento() {
+
+    }
+
+    public TramiteDocumento(int idDocumento, String nombre, Boolean esOpcional, Tramite tramite) {
+        this.idDocumento = idDocumento;
+        this.nombre = nombre;
+        this.esOpcional = esOpcional;
+        this.tramite = tramite;
+    }
+
+    public int getIdDocumento() {
+        return idDocumento;
+    }
+
+    public void setIdDocumento(int idDocumento) {
+        this.idDocumento = idDocumento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Boolean getEsOpcional() {
+        return esOpcional;
+    }
+
+    public void setEsOpcional(Boolean esOpcional) {
+        this.esOpcional = esOpcional;
+    }
+
+    public Tramite getTramite() {
+        return tramite;
+    }
+
+    public void setTramite(Tramite tramite) {
+        this.tramite = tramite;
+    }
 }
