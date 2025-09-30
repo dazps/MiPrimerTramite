6b8ff9fd5695cfa5ec6c92ecf0567e8499880f78
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:10:07 -0500

    Update ITramitePasoRepositories.java

--- src/main/java/pe/edu/upc/miprimertramite/repositories/ITramitePasoRepositories.java
+++ src/main/java/pe/edu/upc/miprimertramite/repositories/ITramitePasoRepositories.java
@@ -1,4 +1,18 @@
 package pe.edu.upc.miprimertramite.repositories;
 
-public interface ITramitePasoRepositories {
+import pe.edu.upc.miprimertramite.entities.TramitePaso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ITramitePasoRepository extends JpaRepository<TramitePaso, Integer> {
+
+    @Query(value = "SELECT * FROM Tramite_Pasos WHERE ID_Tram = ?1", nativeQuery = true)
+    List<TramitePaso> findByTramiteId(int idTramite);
+
+    @Query(value = "SELECT * FROM Tramite_Pasos WHERE ID_Tram = ?1 ORDER BY Orden ASC", nativeQuery = true)
+    List<TramitePaso> findByTramiteIdOrdered(int idTramite);
 }
