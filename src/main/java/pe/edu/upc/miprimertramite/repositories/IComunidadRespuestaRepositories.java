ad8ee83007b01c5f7c21819b8a7fa0844050ad6c
Eder Mauricio <147721558+EderMauricio@users.noreply.github.com>
2025-09-29 23:08:39 -0500

    Update IComunidadRespuestaRepositories.java

--- src/main/java/pe/edu/upc/miprimertramite/repositories/IComunidadRespuestaRepositories.java
+++ src/main/java/pe/edu/upc/miprimertramite/repositories/IComunidadRespuestaRepositories.java
@@ -1,4 +1,18 @@
 package pe.edu.upc.miprimertramite.repositories;
 
-public interface IComunidadRespuestaRepositories {
+import pe.edu.upc.miprimertramite.entities.ComunidadRespuesta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IComunidadRespuestaRepository extends JpaRepository<ComunidadRespuesta, Integer> {
+
+    @Query(value = "SELECT * FROM Comunidad_Respuestas WHERE ID_Pregunta = ?1", nativeQuery = true)
+    List<ComunidadRespuesta> findByPreguntaId(int idPregunta);
+
+    @Query(value = "SELECT COUNT(*) FROM Comunidad_Respuestas WHERE ID_Pregunta = ?1", nativeQuery = true)
+    Long countByPreguntaId(int idPregunta);
 }
